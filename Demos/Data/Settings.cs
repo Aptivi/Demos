9e3cadc0b94a884086481bc04995986df7d7bea8
Aptivi <eoflaoevicecity@gmail.com>
2024-08-15 15:22:22 +0300

    und - Use GHPages indicator
    ---
    
    Type: und
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Demos/Data/Settings.cs
@@ -0,0 +1,13 @@
+﻿namespace Demos.Data
+{
+    public static class Settings
+    {
+        public static bool usesGhPages =
+#if GHPAGES
+            true
+#else
+            false
+#endif
+        ;
+    }
+}
