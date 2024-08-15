945a95798940d6a0a38d521994f3f7e6a6154288
Aptivi <eoflaoevicecity@gmail.com>
2024-08-15 14:41:12 +0300

    chg - Changed base address
    ---
    
    Type: chg
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- Demos/Client/Program.cs
+++ Demos/Client/Program.cs
@@ -14,7 +14,7 @@ namespace Demos.Client
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress + "Demos") });
 
 			// Add necessary services
 			builder.Services.AddSingleton<NamerService>();
