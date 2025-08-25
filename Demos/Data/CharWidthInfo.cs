cd7477ce7d3f9c88394629d82099b07c16b433d6
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-08-25 14:08:24 +0300

    add - Added more Textify text demos
    ---
    
    We've added some more demos for Textify.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Demos/Data/CharWidthInfo.cs
@@ -0,0 +1,18 @@
+﻿using Textify.General.Data;
+
+namespace Demos.Data
+{
+    public class CharWidthInfo
+    {
+        public char Character { get; }
+        public int Width { get; }
+        public CharWidthType Type { get; }
+
+        public CharWidthInfo(char character, int width, CharWidthType type)
+        {
+            Character = character;
+            Width = width;
+            Type = type;
+        }
+    }
+}
