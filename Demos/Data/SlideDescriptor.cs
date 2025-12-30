b7596fec170c29d301db3fa16063b3cb7cd1afe1
Aptivi CEO <eoflaoevicecity@gmail.com>
2025-12-30 10:39:37 +0300

    imp - Ported redesign from official website
    ---
    
    We've ported the redesign from the official website to the Demos website.
    
    ---
    
    Type: imp
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

---
+++ Demos/Data/SlideDescriptor.cs
@@ -0,0 +1,9 @@
+﻿namespace Demos.Data
+{
+    public class SlideDescriptor
+    {
+        public string Picture { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
